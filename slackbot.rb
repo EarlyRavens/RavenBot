161973f7fc3bf5a250f128a539a523491cc934b2
Patrick Tangphao <email": null>
2017-06-13 15:07:32 -0700

    add api request

--- slackbot.rb
+++ slackbot.rb
@@ -1,12 +1,21 @@
 require 'slack-ruby-bot'
+require 'httparty'
 
 class RavenBot < SlackRubyBot::Bot
   command 'ping' do |client, data, _match|
     client.say(text: 'pong', channel: data.channel)
   end
 
+  command 'search' do |client, data, _match|
+    # results = HTTParty.get("https://earlybirdsearch.herokuapp.com/?business=#{expression}&location=94105")
+    results = HTTParty.get("https://earlybirdsearch.herokuapp.com/?business=sushi&location=94105")
+    results.each do |business|
+      client.say(text: "#{business["name"]}", channel: data.channel)
+    end
+  end
+
   command 'about' do |client, data, _match|
-    client.say(text: '#DreamTeam - EarlBird 2017 (github.com/earlyravens) - Max Peiros - (github.com/mpeiros), Mina Melosh (github.com/minamelosh), Earl Sabal (github.com/earlsabal), Helen Khumthong (github.com/tamietta), Patrick Tangphao (github.com/ptangphao)', channel: data.channel)
+    client.say(text: '#DreamTeam - EarlyBird 2017 (github.com/earlyravens) - Max Peiros - (github.com/mpeiros), Mina Melosh (github.com/minamelosh), Earl Sabal (github.com/earlsabal), Helen Khumthong (github.com/tamietta), Patrick Tangphao (github.com/ptangphao)', channel: data.channel)
   end
 end
 
