6f46e877f202b94e33e5152adcc143369b45d0e1
Patrick Tangphao <email": null>
2017-06-13 16:35:12 -0700

    add messages and methods for error handling

--- slackbot.rb
+++ slackbot.rb
@@ -2,9 +2,8 @@ require 'slack-ruby-bot'
 require 'httparty'
 
  QUOTES = ["The early bird gets the worm! - Mina", "There is an art to the process of problem solving - Mike Tarkington", "Efficiency Focused - it's always important to work smart in addition to working hard - Mike Tarkington", "Creative Solutions - even the most difficult challenges can be overcome with creativity and cleverness - Mike Tarkington", "You miss all the shots you don't take - Wayne Gretzky - Michael Scott", "Fart - Omar Cameron", "They Don't Think It Be Like It Is But It Do - Max Peiros", "Has Anyone Really Been Far Even as Decided to Use Even Go Want to do Look More Like? - Patrick Tangphao", "We did the thing - Earl Sabal", "That's Amazing! - Helen Khumthong"]
-
-
 COMMAND_LIST = "Type 'search' followed by a term to search for potential clients based on your search criteria. Raven limits searches to the '94105' zipcode. \n Additional commands: 'about', 'help', 'quote' "
+API_END_POINT = "https://earlybirdsearch.herokuapp.com/"
 
 class RavenBot < SlackRubyBot::Bot
   command 'help' do |client, data, _match|
@@ -16,15 +15,21 @@ class RavenBot < SlackRubyBot::Bot
   end
 
   command 'search' do |client, data, _match|
-    # results = HTTParty.get("https://earlybirdsearch.herokuapp.com/?business=#{expression}&location=94105")
-
-    client.say(text: ARGV[0], channel: data.channel)
-    client.say(text: "Searching for potential clients related to: sushi.\n Please wait 10-15 seconds <@#{data.user}>.", channel: data.channel)
-    results = HTTParty.get("https://earlybirdsearch.herokuapp.com/?business=sushi&location=94105")
-    results["data"].each_with_index do |business, ranking|
-      business_name = business["name"]
-      business_url = business["url"].split('?').first
-      client.say(text: "Result #{ranking + 1}: #{business_name} : #{business_url}", channel: data.channel)
+    begin
+      search_term = _match.to_s.split("search").last
+      client.say(text: "Searching for potential clients related to: #{search_term}\n Please wait 10-15 seconds", channel: data.channel)
+      results = HTTParty.get("#{API_END_POINT}?business=#{search_term}}&location=94105")
+      if results["data"].empty?
+        client.say(text: "No results found.", channel: data.channel)
+      else
+        results["data"].each_with_index do |business, ranking|
+          business_name = business["name"]
+          business_url = business["url"].split('?').first
+          client.say(text: "Result #{ranking + 1}: #{business_name} : #{business_url}", channel: data.channel)
+        end
+      end
+    rescue
+      client.say(text: "An error occurred, please try again later.", channel: data.channel)
     end
   end
 
