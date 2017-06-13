ea9593b5b0cf5e58a442455d528f536998fe4be1
Patrick Tangphao <email": null>
2017-06-13 15:59:02 -0700

    add quote functionality

--- slackbot.rb
+++ slackbot.rb
@@ -1,15 +1,17 @@
 require 'slack-ruby-bot'
 require 'httparty'
 
+QUOTES = ["The early bird gets the worm! - Mina", "There is an art to the process of problem solving - Mike Tarkington", "Efficiency Focused - it's always important to work smart in addition to working hard - Mike Tarkington", "Creative Solutions - even the most difficult challenges can be overcome with creativity and cleverness - Mike Tarkington", "You miss all the shots you don't take - Wayne Gretzky - Michael Scott", "Fart - Omar Cameron", "They Don't Think It Be Like It Is But It Do - Max Peiros", "Has Anyone Really Been Far Even as Decided to Use Even Go Want to do Look More Like? - Patrick Tangphao", "Fuck Elixir - Earl Sabal"]
+
+COMMAND_LIST = "Type 'search' followed by a term to search for potential clients based on your search criteria. Raven limits searches to the '94105' zipcode. \n Additional commands: 'about' , 'quote' "
+
 class RavenBot < SlackRubyBot::Bot
   command 'help' do |client, data, _match|
-    command_list = "Type 'search' followed by a term to search for potential clients based on your search criteria. Raven limits searches to the '94105' zipcode. \n Additional commands: 'about' , 'quote' "
-    client.say(text: command_list, channel: data.channel)
+    client.say(text: COMMAND_LIST, channel: data.channel)
   end
 
   command 'quote' do |client, data, _match|
-    results = HTTParty.get("https://earlybirdsearch.herokuapp.com/")
-    client.say(text: results["quote"], channel: data.channel)
+    client.say(text: QUOTES.sample, channel: data.channel)
   end
 
   command 'search' do |client, data, _match|
