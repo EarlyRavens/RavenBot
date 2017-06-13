59c2626b97f2bdf7fc3b6f158b74269408192c58
Patrick Tangphao <email": null>
2017-06-13 15:58:54 -0700

    add basic search functionality

--- slackbot.rb
+++ slackbot.rb
@@ -2,15 +2,24 @@ require 'slack-ruby-bot'
 require 'httparty'
 
 class RavenBot < SlackRubyBot::Bot
-  command 'ping' do |client, data, _match|
-    client.say(text: 'pong', channel: data.channel)
+  command 'help' do |client, data, _match|
+    command_list = "Type 'search' followed by a term to search for potential clients based on your search criteria. Raven limits searches to the '94105' zipcode. \n Additional commands: 'about' , 'quote' "
+    client.say(text: command_list, channel: data.channel)
+  end
+
+  command 'quote' do |client, data, _match|
+    results = HTTParty.get("https://earlybirdsearch.herokuapp.com/")
+    client.say(text: results["quote"], channel: data.channel)
   end
 
   command 'search' do |client, data, _match|
     # results = HTTParty.get("https://earlybirdsearch.herokuapp.com/?business=#{expression}&location=94105")
+    client.say(text: "Searching for potential clients related to: sushi", channel: data.channel)
     results = HTTParty.get("https://earlybirdsearch.herokuapp.com/?business=sushi&location=94105")
-    results.each do |business|
-      client.say(text: "#{business["name"]}", channel: data.channel)
+    results["data"].each do |business|
+      business_name = business["name"]
+      business_url = business["url"].split('?').first
+      client.say(text: "#{business_name} : #{business_url}", channel: data.channel)
     end
   end
 
